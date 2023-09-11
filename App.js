bba916f917401df733f5922a267811ff6253547b
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-11 10:40:53 +0530

    Created a separate file for scripts and added link to index file

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React using React"
+);
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(heading);

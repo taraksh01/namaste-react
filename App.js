db95b6ca6f5c5d688a0755c452c5870c1549fd1c
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-11 11:41:14 +0530

    Created nested structure of html using react

--- App.js
+++ App.js
@@ -7,3 +7,28 @@ const heading = React.createElement(
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(heading);
+
+/*
+<div id="parent">
+  <div id="child">
+    <h1>I am inside child</h1>
+    <h2>I am sibling of h1 inside child</h2>
+  </div>
+</div> 
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent", style: { color: "orange" } },
+  [
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", {}, "I am inside child1"),
+      React.createElement("h2", {}, "I am sibling of h2 inside child1"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I am inside child2"),
+      React.createElement("h2", {}, "I am sibling of h2 inside child2"),
+    ]),
+  ]
+);
+root.render(parent);

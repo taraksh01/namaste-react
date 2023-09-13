053cfc8f2dd63fbc650a9a05c8940e3fcb2bf2e0
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-09-13 09:39:20 +0530

    Created basic layout for App

---
+++ Potato.js
@@ -0,0 +1,42 @@
+const Potato = () => {
+  return (
+    <div className="potato">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="logo">🥔 Potato</h1>
+      <nav className="nav-bar">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        <input type="text" className="search-box" placeholder="potato" />
+        <p className="search-text">search</p>
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <div className="footer">@taraksh01</div>;
+};
+
+export default Potato;
